fadd869cc6482f5fc1c8c4fda8dbe43041d510fb
mirtris <mirtris0522@gmail.com>
2022-11-22 18:59:05 +0800

    fieat: fizzbuzz done

--- python_leetcode/fizz.py
+++ python_leetcode/fizz.py
@@ -1,14 +1,21 @@
-answer = []
-n = 3
-for i in range(1, n+1):
-    a = ""
-    if(i % 3 == 0):
-        a+="Fizz"
-    if(i % 5 == 0):
-        a+= "Buzz"
-    if a:
-        answer.append(a)
-    else:
-        answer.append(i)
-                
-print(answer)
+class Solution:
+    def __init__(self) -> None:
+        pass
+    
+    def fizzBuzz(n) -> list[str]:
+        answer = []
+        for i in range(1, n+1):
+            a = ''
+            if (i % 3 == 0):
+                a += 'Fizz'
+            if (i % 5 == 0):
+                a += 'Buzz'
+            if a:
+                answer.append(a)
+            else:
+                answer.append(str(i))
+        return answer
+
+
+
+print(Solution.fizzBuzz(30))
